7583705247ab241532f1d2a95dccb52eb723c624
Drobenyuk Andriy <adrobenyuk@shopmonkey.io>
2021-12-28 19:28:40 +0200

    Fixed issue with naming

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,23 +1,37 @@
 const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "1.1.0")));
+app.use(express.static(path.join(__dirname, "canary")));
 
-app.get("/api/login", (req, res) => {
-  res.cookie("USE-CANARY", "enabled", { maxAge: 900000, httpOnly: true });
-  res.send({ id: "user-1", name: "John Dou" });
-});
+function getCanaryAssets() {
+  return Promise.all([
+    fs.promises.readdir(path.join(__dirname, "canary", "static", "js")),
+    fs.promises.readdir(path.join(__dirname, "canary", "static", "css")),
+  ]).then(([scripts, styles]) => ({
+    scripts: scripts.filter(
+      (name) => name.startsWith("main") && name.endsWith(".js")
+    )[0],
+    styles: styles.filter(
+      (name) => name.startsWith("main") && name.endsWith(".css")
+    )[0],
+  }));
+}
+
+app.get("/api/login", (req, res) =>
+  getCanaryAssets().then(({ scripts, styles }) => {
+    res.cookie("USE-CANARY", "enabled", { maxAge: 900000, httpOnly: true });
+    res.send({ id: "user-1", name: "John Dou", assets: { scripts, styles } });
+  })
+);
 
 app.use("*", (req, res) => {
-  console.log("cookies: ");
-  console.log(req.cookies);
-
   if (req.cookies["USE-CANARY"] && req.cookies["USE-CANARY"] === "enabled") {
-    return res.sendFile(path.join(__dirname, "1.1.0", "index.html"));
+    return res.sendFile(path.join(__dirname, "canary", "index.html"));
   }
   return res.sendFile(path.join(__dirname, "public", "index.html"));
 });
