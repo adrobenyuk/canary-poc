173ccfe03c86c1d51d13d57a1997598eb1faa018
Drobenyuk Andriy <adrobenyuk@shopmonkey.io>
2021-12-28 18:59:22 +0200

    Added chunk version

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,27 +1,27 @@
-const path = require('path');
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const path = require("path");
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '1.0.0')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "1.1.0")));
 
-app.get('/api/login', (req, res) => {
-    res.cookie('USE-CANARY', 'enabled', { maxAge: 900000, httpOnly: true })
-    res.send({ id: 'user-1', name: 'John Dou', });
+app.get("/api/login", (req, res) => {
+  res.cookie("USE-CANARY", "enabled", { maxAge: 900000, httpOnly: true });
+  res.send({ id: "user-1", name: "John Dou" });
 });
 
-app.use('*', (req, res) => {
-    console.log('cookies: ');
-    console.log(req.cookies);
+app.use("*", (req, res) => {
+  console.log("cookies: ");
+  console.log(req.cookies);
 
-    if (req.cookies['USE-CANARY'] && req.cookies['USE-CANARY'] === 'enabled') {
-        return res.sendFile(path.join(__dirname, '1.0.0', 'index.html'));
-    }
-    return res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  if (req.cookies["USE-CANARY"] && req.cookies["USE-CANARY"] === "enabled") {
+    return res.sendFile(path.join(__dirname, "1.1.0", "index.html"));
+  }
+  return res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(8888, () => {
-    console.log('Backend in running on http://localhost:8888 ...')
+  console.log("Backend in running on http://localhost:8888 ...");
 });
--- packages/frontend/src/methods/index.js
+++ packages/frontend/src/methods/index.js
@@ -1,29 +1,30 @@
-
 function clearMainStyles() {
-    const styleSheet = Array.from(document.styleSheets).find(({ href }) => href.includes('main'));
-    if (styleSheet) {
-        while (styleSheet.cssRules.length !== 0) {
-            styleSheet.deleteRule(styleSheet.cssRules.length - 1);
-        }
+  const styleSheet = Array.from(document.styleSheets).find(({ href }) =>
+    href.includes("main")
+  );
+  if (styleSheet) {
+    while (styleSheet.cssRules.length !== 0) {
+      styleSheet.deleteRule(styleSheet.cssRules.length - 1);
     }
+  }
 }
 
 function insertNewStyles(pathToFile) {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet'
-    link.href = pathToFile;
-    document.head.appendChild(link);
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = pathToFile;
+  document.head.appendChild(link);
 }
 
 function insertNewScripts(pathToFile) {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = pathToFile;
-    document.head.appendChild(script);
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = pathToFile;
+  document.head.appendChild(script);
 }
 
-export function evalMethod() {
-    clearMainStyles();
-    insertNewStyles('/static/css/main.2de9c4f5.css');
-    insertNewScripts('/static/js/main.2de9c4f5.js');
+export function loadCanary() {
+  clearMainStyles();
+  insertNewStyles("/static/css/main.c34b1999.css");
+  insertNewScripts("/static/js/main.c34b1999.js");
 }
