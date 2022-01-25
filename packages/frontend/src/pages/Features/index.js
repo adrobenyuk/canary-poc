bce21b82710751ba2cf5db40640622b12a35f9a4
Drobenyuk Andriy <adrobenyuk@shopmonkey.io>
2022-01-25 15:06:17 +0200

    made some updates

--- packages/frontend/src/pages/Features/index.js
+++ packages/frontend/src/pages/Features/index.js
@@ -18,6 +18,126 @@ const Features = () => {
             nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
             massa, feugiat ut massa a, dictum malesuada justo.
           </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
+          <p className="fs-5 text-muted">
+            Nulla elementum erat id arcu sagittis, mattis consectetur diam
+            euismod. Interdum et malesuada fames ac ante ipsum primis in
+            faucibus. Nam eget felis porta, aliquam nulla a, rhoncus ante. Donec
+            eget metus non erat dignissim ornare. Curabitur efficitur enim vitae
+            vestibulum egestas. Maecenas viverra dolor nec augue molestie, eu
+            consectetur metus ornare. Nullam feugiat auctor tortor. Quisque et
+            nibh molestie, sollicitudin ipsum quis, porta enim. Vestibulum quam
+            massa, feugiat ut massa a, dictum malesuada justo.
+          </p>
         </div>
       </main>
       <Footer />
